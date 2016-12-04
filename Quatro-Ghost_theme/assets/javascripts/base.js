ddf79fb35397fadae4324c4cf7a03fe8121de4e1
Stefan Batocanin <batocko93@gmail.com>
2016-12-04 19:27:07 +0100

    Fixed nv toggle, home__header height, font-sizes...

--- Quatro-Ghost_theme/assets/javascripts/base.js
+++ Quatro-Ghost_theme/assets/javascripts/base.js
@@ -1,17 +1,7 @@
 $(document).ready(function() {
   $(window).scrollTop(1);
   $(".activate__mobile__nav").click(function() {
-    $(".navigation").addClass("nav-active");
-  });
-  $(".deactivate__mobile__nav").click(function() {
-    $(".navigation").removeClass("nav-active");
-  });
-  $(window).scroll(function() {
-    if($(window).scrollTop() > $(".home__header").outerHeight()) {
-      $(".back__to__home").addClass("back__to__home-visible");
-    }
-    else {
-      $(".back__to__home").removeClass("back__to__home-visible");
-    }
+    $(".activate__mobile__nav").toggleClass("activate__mobile__nav-toggled");
+    $(".navigation").toggleClass("nav-active");
   });
 });
